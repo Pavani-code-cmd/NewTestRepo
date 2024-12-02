1b5fe9a1bd6272b09d426b2b6eb8eb06a3239f2d
Pavani-code-cmd <kittalipavani@gmail.com>
2024-12-02 14:37:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region     = "us-west-2"  # Change this to your desired region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  key_name      = var.key_name
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
